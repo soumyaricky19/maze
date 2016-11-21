4a1c2528dc8de0a6887720c1992812e3d0f1b108
Soumya Mishra <soumya.ricky19@gmail.com>
2016-11-21 01:42:47 -0600

    completed

---
+++ DisjSets.java
@@ -0,0 +1,41 @@
+package maze;
+
+public class DisjSets
+{
+        /** Construct the disjoint sets object.
+         * @param numElements the initial number of disjoint sets.
+         */
+    private int [ ] s;
+    
+    public DisjSets( int numElements )
+    {
+        s = new int [ numElements ];
+        for( int i = 0; i < s.length; i++ )
+        	s[ i ] = -1;
+    }
+
+        /**   Union two disjoint sets.  
+         *     Assume root1 and root2 are distinct and represent set names.
+         * @param root1 the root of set 1.
+         * @param root2 the root of set 2.          */
+        public void union( int root1, int root2 )
+        {
+            s[ root2 ] = root1;
+        }
+        public int find( int x )
+        {
+            if( s[ x ] < 0 )
+                return x;
+            else
+                return find( s[ x ] );
+        }
+        public int findPrev( int x )
+        {
+                return  s[ x ] ;
+        }
+        public void display()
+        {
+        	for( int i = 0; i < s.length; i++ )
+            	System.out.print(s[ i ]+",");
+        }
+}
---
+++ mazeCreator.java
@@ -0,0 +1,145 @@
+package maze;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class mazeCreator 
+{
+	private int length=0,n=0;
+	char s[][];
+	int num=1;
+	mazeCreator(int n)
+	{
+		this.n=n;
+		this.length=2*n+1;
+		s=new char[length][length];
+		init();		
+	}
+	public void init()
+	{
+		for (int i=0;i<length;i++)
+		{
+			for (int j=0;j<length;j++)
+			{
+				if (i%2 == 0)
+				{
+					s[i][j]='-';
+				}
+				else if (i%2 == 1)
+				{
+					if (j%2==0)
+						s[i][j]='|';
+					else 
+						s[i][j]= '0';
+				}	
+			}
+		}
+//		s[0][1]=' ';
+		s[1][0]=' ';
+		s[length-2][length-1]=' ';
+//		s[length-1][length-2]=' ';
+	}
+	
+	public int genRandom(int x)
+	{
+		return (int)(java.lang.Math.random()*100)%x;
+	}
+	
+	public void create()
+	{
+		int cell=0,wall=0,w=0,xCell=0,yCell=0,xWall=0,yWall=0,adjCell=0,count=0,c=0;
+		int prevCell[]={0,0,0};
+		char prevWall = ' ';
+		DisjSets d=new DisjSets(n*n);
+		
+		while ((d.find(n*n-1)) != 0  )
+		{
+			//System.out.println("root of last"+(d.find(n*n-1)));
+			xCell=2*(cell/n+1)-1;
+			yCell=2*(cell%n+1)-1;
+//			System.out.println("cell:"+cell);
+//			System.out.println("xCell"+xCell);
+//			System.out.println("yCell"+yCell);
+			w=genRandom(4);
+			if (w==0)
+			{
+				xWall=xCell-1;
+				yWall=yCell;
+				adjCell=cell-n;
+			}
+			if (w==1)
+			{
+				xWall=xCell;
+				yWall=yCell-1;
+				adjCell=cell-1;
+			}
+			if (w==2)
+			{
+				xWall=xCell+1;
+				yWall=yCell;
+				adjCell=cell+n;
+			}
+			if (w==3)
+			{
+				xWall=xCell;
+				yWall=yCell+1;
+				adjCell=cell+1;
+			}
+			
+			
+			if ((xWall != 2*n && yWall != 2*n) && (xWall != 0 && yWall != 0))
+			{
+				
+				//d.display();
+				System.out.println("w:"+w);
+//				System.out.println("xWall:"+xWall);
+//				System.out.println("yWall:"+yWall);
+				if (s[xWall][yWall] !=' ' && d.find(adjCell)!= 0)
+				{
+					count=0;
+					//prevWall=s[xWall][yWall];
+					s[xWall][yWall]=' ';
+					s[xCell][yCell]=' ';
+//					System.out.println("adjCell:"+adjCell);
+					d.union(cell,adjCell);
+					cell=adjCell;	
+				}			
+				else
+				{
+					count++;
+					if (count>10)
+					{
+						//s[xWall][yWall]=prevWall;
+						cell=d.findPrev(cell);	
+						count=0;
+					}					
+				}
+			}
+		}
+	}
+	
+	public void display()
+	{
+		//System.out.println("L:"+length);
+		for (int i=0;i<length;i++)
+		{
+			for (int j=0;j<length;j++)
+			{
+					System.out.print(s[i][j]);
+			}
+			System.out.println("");
+		}
+	}
+	
+	public static void main(String args[])
+	{
+		int n=0,l;
+		Scanner s= new Scanner(System.in);
+		System.out.println("Please enter the grid size: ");
+		n=s.nextInt();		
+		mazeCreator m=new mazeCreator(n);
+		m.create();
+		m.display();
+	}
+
+}
